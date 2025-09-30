f2d9ea699d8fd878326848cb48103bd4dc90706f
Sangeeth M K <sangeethsmk@gmail.com>
2025-09-30 16:25:50 +0530

    fullscreenonscamblelogo

--- assets/js/critical.js
+++ assets/js/critical.js
@@ -526,3 +526,31 @@ $('#nav-toggle').click(function(){
     $(this).toggleClass('is-active')
     $('ul.nav').toggleClass('show');
 });
+// fullscreen
+const fullscreenbtn = document.getElementById("scramble-logo");
+
+fullscreenbtn.addEventListener("click", () => {
+  if (
+    !document.fullscreenElement &&
+    !document.webkitFullscreenElement &&
+    !document.msFullscreenElement
+  ) {
+    // Enter fullscreen
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    } else if (document.documentElement.webkitRequestFullscreen) { // Safari
+      document.documentElement.webkitRequestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) { // IE11
+      document.documentElement.msRequestFullscreen();
+    }
+  } else {
+    // Exit fullscreen
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { // Safari
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { // IE11
+      document.msExitFullscreen();
+    }
+  }
+});
