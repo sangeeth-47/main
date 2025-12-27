19c9e5d48874fb25fa4288cbd8a0745e11d9d0bd
Sangeeth M K <sangeethsmk@gmail.com>
2025-12-27 20:54:36 +0530

    sectionsadjusted4joboritentedprfl

--- assets/js/sangeeth.js
+++ assets/js/sangeeth.js
@@ -37,7 +37,7 @@ document.addEventListener('click', (e) => {
 })
 
 // Multiple texts for typing effect
-  const texts = ["SERVER ADMINISTRATOR", "DATACENTER ENGINEER" , "FULL STACK DEVELOPER"];
+  const texts = ["SERVER ADMINISTRATOR", "DATACENTER ENGINEER" , "IT INFRA ENGINEER"];
   const speed = 120; // typing speed in ms
   const delay = 1000; // pause before erase
   const target = document.getElementById("srvadmin");
